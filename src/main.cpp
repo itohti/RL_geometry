a481bdd8da6cda9b6d715676631e14f2ccca8bcb
Izdar Tohti <itohti@gmail.com>
2026-01-04 01:26:35 -0500

    update function found we can start getting player data

--- src/main.cpp
+++ src/main.cpp
@@ -36,58 +36,15 @@ class $modify(MyMenuLayer, MenuLayer) {
 	 * `void init()` would not place a hook!
 	*/
 	bool init() {
-		/**
-		 * We call the original init function so that the
-		 * original class is properly initialized.
-		 */
-		if (!MenuLayer::init()) {
+		if (!MenuLayer::init()) 
 			return false;
-		}
 
-		/**
-		 * You can use methods from the `geode::log` namespace to log messages to the console,
-		 * being useful for debugging and such. See this page for more info about logging:
-		 * https://docs.geode-sdk.org/tutorials/logging
-		*/
-		log::debug("Hello from my MenuLayer::init hook! This layer has {} children.", this->getChildrenCount());
+		auto winSize = CCDirector::get()->getWinSize();
+		
+		auto label = CCLabelBMFont::create("WE ARE UP!", "bigFont.fnt");
+		label->setPosition(winSize / 2);
+		this->addChild(label);
 
-		/**
-		 * See this page for more info about buttons
-		 * https://docs.geode-sdk.org/tutorials/buttons
-		*/
-		auto myButton = CCMenuItemSpriteExtra::create(
-			CCSprite::createWithSpriteFrameName("GJ_likeBtn_001.png"),
-			this,
-			/**
-			 * Here we use the name we set earlier for our modify class.
-			*/
-			menu_selector(MyMenuLayer::onMyButton)
-		);
-
-		/**
-		 * Here we access the `bottom-menu` node by its ID, and add our button to it.
-		 * Node IDs are a Geode feature, see this page for more info about it:
-		 * https://docs.geode-sdk.org/tutorials/nodetree
-		*/
-		auto menu = this->getChildByID("bottom-menu");
-		menu->addChild(myButton);
-
-		/**
-		 * The `_spr` string literal operator just prefixes the string with
-		 * your mod id followed by a slash. This is good practice for setting your own node ids.
-		*/
-		myButton->setID("my-button"_spr);
-
-		/**
-		 * We update the layout of the menu to ensure that our button is properly placed.
-		 * This is yet another Geode feature, see this page for more info about it:
-		 * https://docs.geode-sdk.org/tutorials/layouts
-		*/
-		menu->updateLayout();
-
-		/**
-		 * We return `true` to indicate that the class was properly initialized.
-		 */
 		return true;
 	}
 
@@ -98,5 +55,20 @@ class $modify(MyMenuLayer, MenuLayer) {
 	*/
 	void onMyButton(CCObject*) {
 		FLAlertLayer::create("Geode", "Hello from my custom mod!", "OK")->show();
+	}
+};
+
+#include <Geode/modify/PlayLayer.hpp>
+class $modify(MyPlayLayer, PlayLayer) {
+	void startGame() {
+		PlayLayer::startGame();
+
+		log::info("started a level");
+	}
+
+	void postUpdate(float dt) {
+		PlayLayer::postUpdate(dt);
+
+		log::info("update called");
 	}
 };
